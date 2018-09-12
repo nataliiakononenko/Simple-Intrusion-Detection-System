65567886bd3416a3e832eff22c4ada85c8bcb2af
nataliiakononenko <43030449+nataliiakononenko@users.noreply.github.com>
2018-09-12 22:00:23 +0200

    Add files via upload

---
+++ check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+
+bash hash.sh $*
+
+
+
+diff hash.csv copy.csv | sed /-/d | sed '1d' | sed 's/.*;//'
+
---
+++ hash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+
+
+shasum $* | tr -s ' '| tr ' ' ';' > hash.csv
+
+
+
+if  [ -e copy.csv ]
+
+then 
+	
+	:
+
+else
+	 
+	cp hash.csv copy.csv
+ 
+fi
